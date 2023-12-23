12a0cf72d1fa750e1ba0bdcbc4f374950bf1576f
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2023-12-23 07:39:34 +0530

    Create LoadMore.tsx

---
+++ components/LoadMore.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { fetchAnime } from "@/app/action";
+import AnimeCard from "./AnimeCard";
+
+let page = 2;
+
+export type AnimeCard = JSX.Element;
+
+function LoadMore() {
+	const { ref, inView } = useInView();
+	const [data, setdata] = useState<AnimeCard[]>([]);
+
+	useEffect(() => {
+		if (inView) {
+			fetchAnime(page).then((res) => {
+				setdata([...data, ...res]);
+				page += 1;
+			});
+		}
+	}, [data, inView]);
+
+	return (
+		<>
+			<section className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
+				{data}
+			</section>
+			<section className='flex justify-center items-center w-full'>
+				<div ref={ref}>
+					<Image
+						src='./spinner.svg'
+						alt='spinner'
+						width={56}
+						height={56}
+						className='object-contain'
+					/>
+				</div>
+			</section>
+		</>
+	);
+}
+
+export default LoadMore;
